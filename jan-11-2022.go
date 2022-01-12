a9e1e1751b77a5835acca52d94f88fcdb4f085b4
Miracle Nwabueze <m.nwabueze@verifyme.ng>
2022-01-12 08:17:25 +0100

    Rotate array

---
+++ jan-11-2022.go
@@ -0,0 +1,41 @@
+package main
+
+/*
+Given an array, rotate the array to the right by k steps, where k is non-negative.
+
+
+
+Example 1:
+
+Input: nums = [1,2,3,4,5,6,7], k = 3
+Output: [5,6,7,1,2,3,4]
+Explanation:
+rotate 1 steps to the right: [7,1,2,3,4,5,6]
+rotate 2 steps to the right: [6,7,1,2,3,4,5]
+rotate 3 steps to the right: [5,6,7,1,2,3,4]
+
+Example 2:
+
+Input: nums = [-1,-100,3,99], k = 2
+Output: [3,99,-1,-100]
+Explanation:
+rotate 1 steps to the right: [99,-1,-100,3]
+rotate 2 steps to the right: [3,99,-1,-100]
+
+*/
+func main() {
+
+}
+func rotate(nums []int, k int) {
+
+	for i := k; i > 0; i-- {
+
+		lastValue := nums[len(nums)-1]
+
+		for s := len(nums) - 1; s > 0; s-- {
+			nums[s] = nums[s-1]
+		}
+
+		nums[0] = lastValue
+	}
+}
